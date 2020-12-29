378ee622821da3e7d2c17d9ca53077f5fdf15f43
vasyl.maksymet <mcvazin@gmail.com>
2020-12-29 10:18:29 +0200

    ver 29-12__01

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-    $(".pc-close-aside").on("click", function () {
-        $("main").toggleClass("main-pc-full-width");
-        $("aside").toggleClass("aside-pc-close");
-        
-      });
 
   $(".sigma-top-slider").slick({
     infinite: true,
@@ -20,6 +15,26 @@ $(document).ready(function () {
     slidesToScroll: 1,
     arrows: true,
     dots: false,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
   });
 
   $(".sigma-blog-slider").slick({
@@ -28,5 +43,53 @@ $(document).ready(function () {
     slidesToScroll: 1,
     arrows: true,
     dots: false,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
   });
+
+ 
+
+  $(".pc-close-aside").on("click", function () {
+    $("main").toggleClass("main-pc-full-width");
+    $("aside").toggleClass("aside-pc-close");
+  });
+
+
+
+  function highlightFirst(){
+    let flexChildren = document.querySelectorAll('.cat-link');
+    let leftPosition = flexChildren[0].offsetTop;
+    for(let flexChild of flexChildren){
+      if(flexChild.offsetTop <= leftPosition){
+        flexChild.classList.add('firstRow');
+      }else{
+        flexChild.classList.remove('firstRow');
+      }
+    }
+  }
+  window.addEventListener('resize', highlightFirst);
+  
+  $('.sigma-modal').on('shown.bs.modal', function (event) {
+    highlightFirst();
+    $(event.relatedTarget).addClass('openned-catalog');
+  })
+
 }); /*jQuery*/
